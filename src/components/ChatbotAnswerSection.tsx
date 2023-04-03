d6eb59c8a4d7395b2d581789553977a61446793a
shichisan21 <photograph.by@gmail.com>
2023-04-03 23:50:47 +0900

    ボタンD&D操作

--- src/components/ChatbotAnswerSection.tsx
+++ src/components/ChatbotAnswerSection.tsx
@@ -4,21 +4,54 @@ import { getChatGPTAnswer } from "@/utils/getChatGPTAnswer";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
+import { useDrag, useDrop } from "react-dnd";
+import { ItemTypes } from "@/constants/ItemTypes";
+
 interface Props {
   textInput: string;
   setChatGPTAnswer: React.Dispatch<React.SetStateAction<string>>;
+  index: number;
+  moveButton: (dragIndex: number, hoverIndex: number) => void;
 }
 
 export const ChatbotAnswerSection: React.FC<Props> = ({
   textInput,
   setChatGPTAnswer,
+  index,
+  moveButton,
 }) => {
   const [isChatGPTAnserButtonDisabled, setIsChatGPTAnswerButtonDisabled] =
     React.useState(false);
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.BUTTON,
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
+  const [, drop] = useDrop(() => ({
+    accept: ItemTypes.BUTTON,
+    hover(item: any, monitor: any) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveButton(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  }));
+  const opacity = isDragging ? 0.5 : 1;
+
   return (
-    <div>
+    <div ref={drop}>
       <Stack spacing={2} direction='column'>
         <Button
+          ref={drag}
+          id={`{button_${index}}`}
+          draggable='true'
           size='medium'
           variant='outlined'
           type='button'
@@ -39,6 +72,8 @@ export const ChatbotAnswerSection: React.FC<Props> = ({
         </Button>
 
         <Button
+          id='button_02'
+          draggable='true'
           size='medium'
           variant='outlined'
           type='button'
@@ -59,6 +94,8 @@ export const ChatbotAnswerSection: React.FC<Props> = ({
         </Button>
 
         <Button
+          id='button_03'
+          draggable='true'
           size='medium'
           variant='outlined'
           type='button'
